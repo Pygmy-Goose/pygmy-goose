Pygmy-Goose/pygmy-goose

commit e3845113671cc0abd4f2ab6a751abfa1c942fcba

Feature #12699: Window Secondary Sorts
Replace the QuantileSortTree internals with a WindowIndexTree.

src/function/window/window_index_tree.cpp
@@ -7,8 +7,8 @@ namespace duckdb {
 
 WindowIndexTree::WindowIndexTree(ClientContext &context, const BoundOrderModifier &order_bys, vector<column_t> sort_idx,
                                  const idx_t count)
-    : context(context), memory_per_thread(PhysicalOperator::GetMaxThreadMemory(context)),
-      sort_idx(std::move(sort_idx)) {
+    : context(context), memory_per_thread(PhysicalOperator::GetMaxThreadMemory(context)), sort_idx(std::move(sort_idx)),
+      build_stage(PartitionSortStage::INIT), tasks_completed(0) {
 	// Sort the unfiltered indices by the orders
 	LogicalType index_type;
 	if (count < std::numeric_limits<uint32_t>::max()) {
@@ -39,7 +39,12 @@ optional_ptr<LocalSortState> WindowIndexTree::AddLocalSort() {
 	return local_sorts.back().get();
 }
 
+unique_ptr<WindowAggregatorState> WindowIndexTree::GetLocalState() {
+	return make_uniq<WindowIndexTreeLocalState>(*this);
+}
+
 WindowIndexTreeLocalState::WindowIndexTreeLocalState(WindowIndexTree &index_tree) : index_tree(index_tree) {
+	sort_chunk.Initialize(index_tree.context, index_tree.global_sort->sort_layout.logical_types);
 	payload_chunk.Initialize(index_tree.context, index_tree.global_sort->payload_layout.GetTypes());
 	local_sort = index_tree.AddLocalSort();
 }
@@ -51,6 +56,7 @@ void WindowIndexTreeLocalState::SinkChunk(DataChunk &chunk, const idx_t row_idx,
 	for (column_t c = 0; c < sort_idx.size(); ++c) {
 		sort_chunk.data[c].Reference(chunk.data[sort_idx[c]]);
 	}
+	sort_chunk.SetCardinality(chunk);
 
 	//	Sequence the payload column
 	auto &indices = payload_chunk.data[0];
@@ -72,12 +78,12 @@ void WindowIndexTreeLocalState::SinkChunk(DataChunk &chunk, const idx_t row_idx,
 }
 
 void WindowIndexTreeLocalState::ExecuteSortTask() {
-	auto &global_sort = *index_tree.global_sort;
 	switch (build_stage) {
 	case PartitionSortStage::SCAN:
-		global_sort.AddLocalState(*index_tree.local_sorts[build_task]);
+		index_tree.global_sort->AddLocalState(*index_tree.local_sorts[build_task]);
 		break;
 	case PartitionSortStage::MERGE: {
+		auto &global_sort = *index_tree.global_sort;
 		MergeSorter merge_sorter(global_sort, global_sort.buffer_manager);
 		merge_sorter.PerformInMergeRound();
 		break;
@@ -117,24 +123,26 @@ void WindowIndexTreeLocalState::BuildLeaves() {
 		return;
 	}
 
-	PayloadScanner scanner(global_sort, build_task, true);
+	PayloadScanner scanner(global_sort, build_task);
+	idx_t row_idx = index_tree.block_starts[build_task];
 	for (;;) {
-		idx_t row_idx = scanner.Scanned();
 		payload_chunk.Reset();
 		scanner.Scan(payload_chunk);
-		if (payload_chunk.size() == 0) {
+		const auto count = payload_chunk.size();
+		if (count == 0) {
 			break;
 		}
 		auto &indices = payload_chunk.data[0];
 		if (index_tree.mst32) {
 			auto &sorted = index_tree.mst32->LowestLevel();
 			auto data = FlatVector::GetData<uint32_t>(indices);
-			std::copy(data, data + payload_chunk.size(), sorted.data() + row_idx);
+			std::copy(data, data + count, sorted.data() + row_idx);
 		} else {
 			auto &sorted = index_tree.mst64->LowestLevel();
 			auto data = FlatVector::GetData<uint64_t>(indices);
-			std::copy(data, data + payload_chunk.size(), sorted.data() + row_idx);
+			std::copy(data, data + count, sorted.data() + row_idx);
 		}
+		row_idx += count;
 	}
 }
 
@@ -228,7 +236,7 @@ bool WindowIndexTree::TryPrepareSortStage(WindowIndexTreeLocalState &lstate) {
 	return true;
 }
 
-void WindowIndexTreeLocalState::Build() {
+void WindowIndexTreeLocalState::Sort() {
 	// Sort, merge and build the tree in parallel
 	while (index_tree.build_stage.load() != PartitionSortStage::FINISHED) {
 		if (index_tree.TryPrepareSortStage(*this)) {
@@ -237,20 +245,21 @@ void WindowIndexTreeLocalState::Build() {
 			std::this_thread::yield();
 		}
 	}
+}
 
-	//	Now build the tree in parallel
-	if (index_tree.mst32) {
-		index_tree.mst32->Build();
+void WindowIndexTree::Build() {
+	if (mst32) {
+		mst32->Build();
 	} else {
-		index_tree.mst64->Build();
+		mst64->Build();
 	}
 }
 
 idx_t WindowIndexTree::SelectNth(const SubFrames &frames, idx_t n) const {
 	if (mst32) {
-		return mst32->SelectNth(frames, n);
+		return mst32->NthElement(mst32->SelectNth(frames, n));
 	} else {
-		return mst64->SelectNth(frames, n);
+		return mst64->NthElement(mst32->SelectNth(frames, n));
 	}
 }
 

src/include/duckdb/execution/merge_sort_tree.hpp
@@ -253,6 +253,7 @@ protected:
 		return level;
 	}
 
+public:
 	void Print() const {
 		std::ostringstream out;
 		const char *separator = "    ";

src/include/duckdb/function/window/window_index_tree.hpp
@@ -15,8 +15,8 @@ public:
 
 	//! Add a chunk to the local sort
 	void SinkChunk(DataChunk &chunk, const idx_t row_idx, optional_ptr<SelectionVector> filter_sel, idx_t filtered);
-	//! Build the MST from the sorted data
-	void Build();
+	//! Sort the data
+	void Sort();
 	//! Process sorted leaf data
 	void BuildLeaves();
 
@@ -46,7 +46,7 @@ public:
 	                const idx_t count);
 	virtual ~WindowIndexTree() = default;
 
-	unique_ptr<WindowAggregatorState> GetLocalState() const;
+	unique_ptr<WindowAggregatorState> GetLocalState();
 
 	//! Make a local sort for a thread
 	optional_ptr<LocalSortState> AddLocalSort();
@@ -55,6 +55,9 @@ public:
 	bool TryPrepareSortStage(WindowIndexTreeLocalState &lstate);
 	//! Thread-safe post-sort cleanup
 	void CleanupSort();
+
+	//! Build the MST in parallel from the sorted data
+	void Build();
 
 	//! Find the Nth index in the set of subframes
 	idx_t SelectNth(const SubFrames &frames, idx_t n) const;
@@ -76,9 +79,11 @@ public:
 	//! Tasks launched
 	idx_t total_tasks = 0;
 	//! Tasks launched
-	idx_t tasks_assigned;
+	idx_t tasks_assigned = 0;
 	//! Tasks landed
 	atomic<idx_t> tasks_completed;
+	//! The block starts (the scanner doesn't know this) plus the total count
+	vector<idx_t> block_starts;
 
 	// Merge sort trees for various sizes
 	// Smaller is probably not worth the effort.
@@ -90,8 +95,6 @@ public:
 private:
 	//! Find the starts of all the blocks
 	void MeasurePayloadBlocks();
-	//! The block starts (the scanner doesn't know this) plus the total count
-	vector<idx_t> block_starts;
 };
 
 } // namespace duckdb

commit c76fdb3917955fcc65f8ad6bb7c1b120a3523c7d

window: fix nullptr dereference The bad copy-paste was the cause of a potential nullptr dereference. Found by Postgres Pro Fixes: e384511367 ("Feature #12699: Window Secondary Sorts") Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

src/function/window/window_index_tree.cpp
@@ -56,7 +56,7 @@ idx_t WindowIndexTree::SelectNth(const SubFrames &frames, idx_t n) const {
 	if (mst32) {
 		return mst32->NthElement(mst32->SelectNth(frames, n));
 	} else {
-		return mst64->NthElement(mst32->SelectNth(frames, n));
+		return mst64->NthElement(mst64->SelectNth(frames, n));
 	}
 }
 