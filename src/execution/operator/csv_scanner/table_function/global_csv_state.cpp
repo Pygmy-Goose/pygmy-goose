Pygmy-Goose/pygmy-goose

commit a02a34f791988cf326d5fcbdcba9ac369c4feb5d

more small fix and test adjustment

src/execution/operator/csv_scanner/table_function/global_csv_state.cpp
@@ -25,7 +25,12 @@ CSVGlobalState::CSVGlobalState(ClientContext &context_p, const shared_ptr<CSVBuf
 		// If not we need to construct it for the first file
 		file_scans.emplace_back(
 		    make_uniq<CSVFileScan>(context, files[0], options, 0U, bind_data, column_ids, file_schema, false));
-	};
+	}
+	idx_t cur_file_idx = 0;
+	while (file_scans.back()->start_iterator.done && file_scans.size() < files.size()) {
+		file_scans.emplace_back(make_uniq<CSVFileScan>(context, files[++cur_file_idx], options, 0U, bind_data,
+		                                               column_ids, file_schema, false));
+	}
 	// There are situations where we only support single threaded scanning
 	bool many_csv_files = files.size() > 1 && files.size() > system_threads * 2;
 	single_threaded = many_csv_files || !options.parallel;

test/sql/copy/csv/glob/read_csv_glob.test
@@ -29,7 +29,7 @@ VARCHAR	BOOLEAN	DOUBLE	DOUBLE	VARCHAR
 
 # All varchar because we force c2 to be the sniffed file
 query IIIII
-select typeof(#1),typeof(#2),typeof(#3),typeof(#4),typeof(#5) FROM read_csv(['data/csv/per_thread/c2.csv', 'data/csv/per_thread/c1.csv']) limit 1
+select typeof(#1),typeof(#2),typeof(#3),typeof(#4),typeof(#5) FROM read_csv(['data/csv/per_thread/c2.csv', 'data/csv/per_thread/c1.csv', 'data/csv/per_thread/c3.csv']) limit 1
 ----
 VARCHAR	VARCHAR	VARCHAR	VARCHAR	VARCHAR
 

commit f92e0895b63b109d566ff6cac2f901fcc58bdc1b

csv_scanner: fix order of evaluation of arguments to a function Replaced computing of the variable "cur_file_idx". The order of evaluation of arguments to a function is unspecified(ISO IEC 14882-2011:1.9.3) Found by Postgres Pro Fixes: a02a34f791 ("more small fix and test adjustment") Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

src/execution/operator/csv_scanner/table_function/global_csv_state.cpp
@@ -28,7 +28,8 @@ CSVGlobalState::CSVGlobalState(ClientContext &context_p, const shared_ptr<CSVBuf
 	}
 	idx_t cur_file_idx = 0;
 	while (file_scans.back()->start_iterator.done && file_scans.size() < files.size()) {
-		file_scans.emplace_back(make_uniq<CSVFileScan>(context, files[++cur_file_idx], options, cur_file_idx, bind_data,
+		cur_file_idx++;
+		file_scans.emplace_back(make_uniq<CSVFileScan>(context, files[cur_file_idx], options, cur_file_idx, bind_data,
 		                                               column_ids, file_schema, false));
 	}
 	// There are situations where we only support single threaded scanning