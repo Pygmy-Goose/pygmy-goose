dentiny/duckdb

update impl based on lauren's suggestion

src/include/duckdb/storage/buffer/buffer_pool.hpp
@@ -91,6 +91,9 @@ protected:
 	virtual EvictionResult EvictBlocksInternal(EvictionQueue &queue, MemoryTag tag, idx_t extra_memory,
 	                                           idx_t memory_limit, unique_ptr<FileBuffer> *buffer = nullptr);
 
+	//! Evict object cache entries if needed.
+	EvictionResult EvictObjectCacheEntriesIfNeeded(MemoryTag tag, idx_t extra_memory, idx_t memory_limit);
+
 	//! Purge all blocks that haven't been pinned within the last N seconds
 	idx_t PurgeAgedBlocks(uint32_t max_age_sec);
 	idx_t PurgeAgedBlocksInternal(EvictionQueue &queue, uint32_t max_age_sec, int64_t now, int64_t limit);

Pygmy-Goose/pygmy-goose

src/include/duckdb/storage/buffer/buffer_pool.hpp
@@ -91,6 +91,9 @@ protected:
 	virtual EvictionResult EvictBlocksInternal(EvictionQueue &queue, MemoryTag tag, idx_t extra_memory,
 	                                           idx_t memory_limit, unique_ptr<FileBuffer> *buffer = nullptr);
 
+	//! Evict object cache entries if needed.
+	EvictionResult EvictObjectCacheEntries(MemoryTag tag, idx_t extra_memory, idx_t memory_limit);
+
 	//! Purge all blocks that haven't been pinned within the last N seconds
 	idx_t PurgeAgedBlocks(uint32_t max_age_sec);
 	idx_t PurgeAgedBlocksInternal(EvictionQueue &queue, uint32_t max_age_sec, int64_t now, int64_t limit);