duckdb/duckdb

Add odbc_scanner as a core extension
This PR adds [odbc_scanner](https://github.com/duckdb/odbc-scanner) as
an out-of-tree core extension. This allows to build and distribute it
the same way as it is currently done for Postgres, MySQL and SQLite
extensions.

`odbc_scanner` is added to various registries (alias, auto-load) next to
the corresponding `mysql_scanner` entries.

Extension entries (only functions) for it are generated using
`generate_extensions_function.py`.

Support for building `odbc_scanner` this way (on this repo CI) was added
in duckdb/odbc-scanner#106 .

Supersedes: #20560

Ref: duckdblabs/duckdb-internal#7185

src/include/duckdb/main/extension_entries.hpp
@@ -476,6 +476,17 @@ static constexpr ExtensionFunctionEntry EXTENSION_FUNCTIONS[] = {
     {"nextafter", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"normalized_interval", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"now", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_begin_transaction", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_bind_params", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_close", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_commit", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_connect", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_copy_from", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_create_params", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_list_data_sources", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_list_drivers", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_query", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_rollback", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"ord", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"parquet_bloom_probe", "parquet", CatalogType::TABLE_FUNCTION_ENTRY},
     {"parquet_file_metadata", "parquet", CatalogType::TABLE_FUNCTION_ENTRY},
@@ -1184,30 +1195,18 @@ static constexpr ExtensionEntry EXTENSION_SECRET_PROVIDERS[] = {
     {"mysql/config", "mysql_scanner"},
     {"postgres/config", "postgres_scanner"}}; // EXTENSION_SECRET_PROVIDERS
 
-static constexpr const char *AUTOLOADABLE_EXTENSIONS[] = {"avro",
-                                                          "aws",
-                                                          "azure",
-                                                          "autocomplete",
-                                                          "core_functions",
-                                                          "delta",
-                                                          "ducklake",
-                                                          "encodings",
-                                                          "excel",
-                                                          "fts",
-                                                          "httpfs",
-                                                          "iceberg",
-                                                          "inet",
-                                                          "icu",
-                                                          "json",
-                                                          "motherduck",
-                                                          "mysql_scanner",
-                                                          "parquet",
-                                                          "sqlite_scanner",
-                                                          "sqlsmith",
-                                                          "postgres_scanner",
-                                                          "tpcds",
-                                                          "tpch",
-                                                          "uc_catalog",
-                                                          "ui"}; // END_OF_AUTOLOADABLE_EXTENSIONS
+static constexpr const char *AUTOLOADABLE_EXTENSIONS[] = {"avro",           "aws",
+                                                          "azure",          "autocomplete",
+                                                          "core_functions", "delta",
+                                                          "ducklake",       "encodings",
+                                                          "excel",          "fts",
+                                                          "httpfs",         "iceberg",
+                                                          "inet",           "icu",
+                                                          "json",           "motherduck",
+                                                          "mysql_scanner",  "odbc_scanner",
+                                                          "parquet",        "sqlite_scanner",
+                                                          "sqlsmith",       "postgres_scanner",
+                                                          "tpcds",          "tpch",
+                                                          "uc_catalog",     "ui"}; // END_OF_AUTOLOADABLE_EXTENSIONS
 
 } // namespace duckdb

src/main/extension/extension_helper.cpp
@@ -112,6 +112,7 @@ static const DefaultExtension internal_extensions[] = {
     {"autocomplete", "Adds support for autocomplete in the shell", DUCKDB_EXTENSION_AUTOCOMPLETE_LINKED},
     {"motherduck", "Enables motherduck integration with the system", false},
     {"mysql_scanner", "Adds support for connecting to a MySQL database", false},
+    {"odbc_scanner", "Adds support for connecting to remote databases over ODBC", false},
     {"sqlite_scanner", "Adds support for reading and writing SQLite database files", false},
     {"postgres_scanner", "Adds support for connecting to a Postgres database", false},
     {"inet", "Adds support for IP-related data types and functions", false},
@@ -143,8 +144,9 @@ DefaultExtension ExtensionHelper::GetDefaultExtension(idx_t index) {
 // Allow Auto-Install Extensions
 //===--------------------------------------------------------------------===//
 static const char *const auto_install[] = {
-    "motherduck", "postgres_scanner", "mysql_scanner", "sqlite_scanner", "delta", "iceberg", "uc_catalog",
-    "ui",         "ducklake",         nullptr};
+    "motherduck", "postgres_scanner", "mysql_scanner", "odbc_scanner", "sqlite_scanner",
+    "delta",      "iceberg",          "uc_catalog",    "ui",           "ducklake",
+    nullptr};
 
 // TODO: unify with new autoload mechanism
 bool ExtensionHelper::AllowAutoInstall(const string &extension) {

Pygmy-Goose/pygmy-goose

src/include/duckdb/main/extension_entries.hpp
@@ -478,6 +478,17 @@ static constexpr ExtensionFunctionEntry EXTENSION_FUNCTIONS[] = {
     {"nextafter", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"normalized_interval", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"now", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_begin_transaction", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_bind_params", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_close", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_commit", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_connect", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_copy", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_create_params", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
+    {"odbc_list_data_sources", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_list_drivers", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_query", "odbc_scanner", CatalogType::TABLE_FUNCTION_ENTRY},
+    {"odbc_rollback", "odbc_scanner", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"ord", "core_functions", CatalogType::SCALAR_FUNCTION_ENTRY},
     {"parquet_bloom_probe", "parquet", CatalogType::TABLE_FUNCTION_ENTRY},
     {"parquet_file_metadata", "parquet", CatalogType::TABLE_FUNCTION_ENTRY},
@@ -1183,30 +1194,18 @@ static constexpr ExtensionEntry EXTENSION_SECRET_PROVIDERS[] = {
     {"mysql/config", "mysql_scanner"},
     {"postgres/config", "postgres_scanner"}}; // EXTENSION_SECRET_PROVIDERS
 
-static constexpr const char *AUTOLOADABLE_EXTENSIONS[] = {"avro",
-                                                          "aws",
-                                                          "azure",
-                                                          "autocomplete",
-                                                          "core_functions",
-                                                          "delta",
-                                                          "ducklake",
-                                                          "encodings",
-                                                          "excel",
-                                                          "fts",
-                                                          "httpfs",
-                                                          "iceberg",
-                                                          "inet",
-                                                          "icu",
-                                                          "json",
-                                                          "motherduck",
-                                                          "mysql_scanner",
-                                                          "parquet",
-                                                          "sqlite_scanner",
-                                                          "sqlsmith",
-                                                          "postgres_scanner",
-                                                          "tpcds",
-                                                          "tpch",
-                                                          "unity_catalog",
-                                                          "ui"}; // END_OF_AUTOLOADABLE_EXTENSIONS
+static constexpr const char *AUTOLOADABLE_EXTENSIONS[] = {"avro",           "aws",
+                                                          "azure",          "autocomplete",
+                                                          "core_functions", "delta",
+                                                          "ducklake",       "encodings",
+                                                          "excel",          "fts",
+                                                          "httpfs",         "iceberg",
+                                                          "inet",           "icu",
+                                                          "json",           "motherduck",
+                                                          "mysql_scanner",  "odbc_scanner",
+                                                          "parquet",        "sqlite_scanner",
+                                                          "sqlsmith",       "postgres_scanner",
+                                                          "tpcds",          "tpch",
+                                                          "unity_catalog",  "ui"}; // END_OF_AUTOLOADABLE_EXTENSIONS
 
 } // namespace duckdb

src/main/extension/extension_helper.cpp
@@ -112,6 +112,7 @@ static const DefaultExtension internal_extensions[] = {
     {"autocomplete", "Adds support for autocomplete in the shell", DUCKDB_EXTENSION_AUTOCOMPLETE_LINKED},
     {"motherduck", "Enables motherduck integration with the system", false},
     {"mysql_scanner", "Adds support for connecting to a MySQL database", false},
+    {"odbc_scanner", "Adds support for connecting to remote databases over ODBC", false},
     {"sqlite_scanner", "Adds support for reading and writing SQLite database files", false},
     {"postgres_scanner", "Adds support for connecting to a Postgres database", false},
     {"inet", "Adds support for IP-related data types and functions", false},
@@ -143,8 +144,9 @@ DefaultExtension ExtensionHelper::GetDefaultExtension(idx_t index) {
 // Allow Auto-Install Extensions
 //===--------------------------------------------------------------------===//
 static const char *const auto_install[] = {
-    "motherduck", "postgres_scanner", "mysql_scanner", "sqlite_scanner", "delta", "iceberg", "unity_catalog",
-    "ui",         "ducklake",         nullptr};
+    "motherduck", "postgres_scanner", "mysql_scanner", "odbc_scanner", "sqlite_scanner",
+    "delta",      "iceberg",          "unity_catalog", "ui",           "ducklake",
+    nullptr};
 
 // TODO: unify with new autoload mechanism
 bool ExtensionHelper::AllowAutoInstall(const string &extension) {